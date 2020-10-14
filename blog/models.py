b663ab59e6be366369c00dc868dc048690c13da8
Spencer Kasteler <sdkasteler@gmail.com>
2020-10-14 13:36:12 -0600

    set up migrations to make the database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    blog_title = models.CharField(max_length=100)
+    blog_author = models.CharField(max_length=100)
+    blog_content = models.TextField()
+    blog_date = models.DateTimeField('date published')
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_author = models.CharField(max_length=100)
+    comment_email = models.EmailField()
+    comment_content = models.TextField()
+    comment_date = models.DateTimeField('date published')
