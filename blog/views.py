3934504c423a8b7ddc59d41baede7d68c38f7596
Spencer Kasteler <sdkasteler@gmail.com>
2020-10-14 13:02:52 -0600

    finished the 2 mostly static pages. tech tips and about

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+def about(request):
+    now = datetime.now()
+    context = {'time': now, }
+    return render(request, 'blog/about.html', context)
+
+def tips(request):
+    now = datetime.now()
+    context = {'time': now, }
+    return render(request, 'blog/tips.html', context)
